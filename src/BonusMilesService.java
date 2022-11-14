4cbad7182697313e9e8cf7953ae8aea6cd89190d
Ravendrm <bkforsner@gmail.com>
2022-11-14 15:21:41 +0300

    final changes

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,14 +1,8 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-                int price = 35000 ; //стоимость билета
+        int BonusMilesCalc = cost / 20;
 
-                int milePrice = 20 ; //стоимость 1 мили
-                int MilesEarned = (price / milePrice);
-
-                System.out.println(price + " стоимость билета ") ;
-                System.out.println(MilesEarned + " начислено миль за покупку билета ") ;
-
-        return MilesEarned;
+        return BonusMilesCalc;
     }
 
 }
